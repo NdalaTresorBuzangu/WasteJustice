17084e373574f22ca8748bc66f2a82c5293f98a7
Tresorndala <ndalabuzangu@gmail.com>
2025-11-26 10:01:40 +0000

    Initial commit of register_sample project

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header("Location: login.php?logout=true"); // Redirect to login with a logout notice
-exit;
-?>
